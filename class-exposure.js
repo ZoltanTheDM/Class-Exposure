14e5c2b5b4e477d271c66377d3f57ec081c4abc4
Zoltan The DM <zoltanthedm@gmail.com>
2021-08-05 14:52:19 -0700

    ready for 1.0.0

--- class-exposure.js
+++ class-exposure.js
@@ -1,10 +1,11 @@
 
 const CLASS_STORAGE = "stored-classes";
 const MODULE_NAME = "class-exposure";
+const ENGLISH_MODULE_NAME = "Class Exposure";
 
 Hooks.once('ready', function(){
 	if (!CONFIG.DND5E.classFeatures){
-		console.error("Somehow class Features is missing!");
+		console.error(`${ENGLISH_MODULE_NAME} | Somehow classFeatures is missing!`);
 		return;
 	}
 
@@ -14,6 +15,7 @@ Hooks.once('ready', function(){
 		scope: 'world',
 		config: false,
 		type: Object,
+		//create deep copy
 		default: JSON.parse(JSON.stringify(CONFIG.DND5E.classFeatures))
 	});
 
@@ -21,6 +23,18 @@ Hooks.once('ready', function(){
 
 	//change classFeature to use saved class feature
 	CONFIG.DND5E.classFeatures = item;
+
+	//classFeatures is a dictionary with lower case class names as keys
+	//each class is a dictionary with 2 keys: "features" and "subclasses"
+	//features is an dictionary with levels numbers as keys to an array of strings
+	//strings are Uuid
+	//uuid from compedium: "Compendium.{name from game.packs}.{item id}"
+	//uuid from instances "Item.{item id}"
+	//subclasses are a dictionary has a key similar to name of subclass
+	//the dictionary contains the key "label", "source", "features"
+	//features is identical structure to class features
+	//label is the exact string name used for that subclass
+	//source is a string
 
 	// game.settings.set(MODULE_NAME, CLASS_STORAGE, item);
 
